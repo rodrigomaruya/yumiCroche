32bb79907ded84a12ddd86b5085752c81344d65e
Rodrigo <rymaruya@gmail.com>
2024-08-20 14:01:11 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Header from "./Header/Header";
 import Products from "./Products/Products";
 import Cart from "./Cart/Cart";
-
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
       <Header />
       <Products />
       <Cart />
+      <ToastContainer />
     </>
   );
 }
--- src/CardProduct/CardProduct.tsx
+++ src/CardProduct/CardProduct.tsx
@@ -2,6 +2,8 @@ import './CardProduct.css'
 import { FaCartPlus } from "react-icons/fa";
 import { useStore } from '../store/store';
 import { formatCurrency } from '../formatCurrency';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 type Product = {
   data: {
     id: number
@@ -14,23 +16,37 @@ type Product = {
 }
 
 export default function CardProduct({data}: Product) {
-  const { setCartProduct,CartProduct,setOpen} = useStore()
+  const { setCartProduct,CartProduct} = useStore()
+
+  const notify = () => toast.success('Item adicionado ao carrinho!',{
+    position: "bottom-center",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'dark',
+  });
   const handleAdd = () => {
     const existingProduct = CartProduct.find(item => item.id === data.id);
     if (existingProduct) {
       existingProduct.quantity++;
       setCartProduct([...CartProduct]);
+      notify();
       return;
     }else{
-
+      
       setCartProduct([...CartProduct,data])
+      notify();
     }
    
     
+    
   }
-
+  
   return (
-    <div className="CardProduct" onClick={()=>setOpen(false)}>
+    <div className="CardProduct" >
       <img 
         src={data.src} 
         alt="Image" 
@@ -42,7 +58,7 @@ export default function CardProduct({data}: Product) {
       </div>
       <button className='CardProduct-button' onClick={handleAdd}>
         <FaCartPlus />
-
+        
       </button>
     </div>
   )
